BrianJairam/DiscordTradingSim

commit 3307e2b2c56f0eb1a05abbcb04837a0353a98f63

Refactor to follow flake8

bank.py
@@ -3,18 +3,22 @@ import datetime
 import discord
 import os
 import schedule
-import time
 import sqlite3
+import time
+
 import economy_functions as ef
+
 from dotenv import load_dotenv
 
-deposit_rate = 0.01
-lending_rate = 0.02
+DEPOSIT_RATE = 0.01
+LENDING_RATE = 0.02
+
 
 def bank_balance(user):
     db = sqlite3.connect('main.sqlite')
     cursor = db.cursor()
-    cursor.execute('SELECT dollars FROM bank_deposits WHERE user_id = ?', (user.id,))
+    cursor.execute('SELECT dollars FROM bank_deposits WHERE user_id = ?',
+                   (user.id,))
     result = cursor.fetchone()
     if result is None:
         sql = ("INSERT INTO bank_deposits (user_id, dollars) VALUES(?, ?)")
@@ -28,6 +32,7 @@ def bank_balance(user):
     db.close()
     return f'{user.name} has {ans:.2f} dollars in deposits.'
 
+
 def new_deposit(user, amount, guild_id):
     if (amount <= 0):
         return "Please enter a positive amount."
@@ -36,7 +41,8 @@ def new_deposit(user, amount, guild_id):
     else:
         db = sqlite3.connect('main.sqlite')
         cursor = db.cursor()
-        cursor.execute('SELECT dollars FROM bank_deposits WHERE user_id = ?', (user.id,))
+        cursor.execute('SELECT dollars FROM bank_deposits WHERE user_id = ?',
+                       (user.id,))
         result = cursor.fetchone()
         if result is None:
             sql = ("INSERT INTO bank_deposits (user_id, dollars) VALUES(?, ?)")
@@ -54,13 +60,15 @@ def new_deposit(user, amount, guild_id):
     ef.money_transfer(user.id, -amount)
     return f"{user.name} deposited {amount:.2f} dollars."
 
+
 def new_withdrawal(user, amount, guild_id):
     if (amount <= 0):
         return "Please enter a positive amount."
     else:
         db = sqlite3.connect('main.sqlite')
         cursor = db.cursor()
-        cursor.execute('SELECT dollars FROM bank_deposits WHERE user_id = ?', (user.id,))
+        cursor.execute('SELECT dollars FROM bank_deposits WHERE user_id = ?',
+                       (user.id,))
         result = cursor.fetchone()
         if (result is None or result[0] < amount):
             cursor.close()
@@ -74,24 +82,24 @@ def new_withdrawal(user, amount, guild_id):
             db.commit()
             cursor.close()
             db.close()
-            ef.ledger_update("Bank_Withdrawal", guild_id, "\"Bank\"", user.id, amount)
+            ef.ledger_update("Bank_Withdrawal", guild_id,
+                             "\"Bank\"", user.id, amount)
             ef.money_transfer(user.id, amount)
             ef.money_transfer("\"Bank\"", -amount)
             return f"{user.name} has withdrawn {amount:.2f} dollars."
 
+
 def handle_interest():
-        with open("date.txt", "r") as f:
-            current_day = f.readline()
-            current_day = datetime.datetime.strptime(current_day, '%m/%d/%Y').date()
-            if (datetime.date.today() != current_day):
-                d = (datetime.date.today() - current_day).days
-                print(f"Paying interest for {d} days")
-                db = sqlite3.connect('main.sqlite')
-                cursor = db.cursor()
-                cursor.execute(f'UPDATE bank_deposits SET dollars = dollars * {(1 + deposit_rate / 365) ** d}')
-                db.commit()
-                with open("date.txt", "w+") as f:
-                    f.write(datetime.date.today().strftime('%m/%d/%Y'))
-
-
-
+    with open("date.txt", "r") as f:
+        current_day = datetime.datetime.strptime(f.readline(),
+                                                 '%m/%d/%Y').date()
+        if (datetime.date.today() != current_day):
+            d = (datetime.date.today() - current_day).days
+            print(f"Paying interest for {d} days")
+            db = sqlite3.connect('main.sqlite')
+            cursor = db.cursor()
+            cursor.execute(f'UPDATE bank_deposits SET dollars = \
+                             dollars * {(1 + DEPOSIT_RATE / 365) ** d}')
+            db.commit()
+            with open("date.txt", "w+") as f:
+                f.write(datetime.date.today().strftime('%m/%d/%Y'))

bot.py
@@ -1,12 +1,14 @@
 import asyncio
+import bank
 import discord
 import os
+import random
 import schedule
 import sqlite3
-import random
-import bank
-import economy_functions as ef
 import trading
+
+import economy_functions as ef
+
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -15,7 +17,8 @@ version_num = "Alpha 1.1"
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = commands.Bot(command_prefix = '$')
+client = commands.Bot(command_prefix='$')
+
 
 @client.event
 async def on_ready():
@@ -77,16 +80,15 @@ async def on_ready():
 
     print("DiscordTradingSim is ready.")
 
-# General Commands 
+# General Commands
 
 # $version
-
 @client.command()
 async def version(ctx):
     await ctx.send(f"{version_num}")
 
-# $dice
 
+# $dice
 @client.command()
 async def dice(ctx, choice):
     faces = [1, 2, 3, 4, 5, 6]
@@ -96,12 +98,14 @@ async def dice(ctx, choice):
     else:
         result = random.choice(faces)
         if (choice == result):
-            ef.ledger_update("Gambling", ctx.guild.id, "\"Casino\"", ctx.message.author.id, 0.01)
+            ef.ledger_update("Gambling", ctx.guild.id,
+                             "\"Casino\"", ctx.message.author.id, 0.01)
             ef.money_transfer(ctx.message.author.id, 0.01)
             ef.money_transfer("\"Casino\"", -0.01)
             await ctx.send(f'Rolled a {result}. You win $0.01!')
         else:
             await ctx.send(f'Rolled a {result}. You lose!')
+
 
 @client.command()
 async def wagered_dice(ctx, choice, bet: float):
@@ -117,23 +121,27 @@ async def wagered_dice(ctx, choice, bet: float):
         result = random.choice(faces)
         if (choice == result):
             winnings = bet * 2
-            ef.ledger_update("Gambling", ctx.guild.id, "\"Casino\"", ctx.message.author.id, winnings)
+            ef.ledger_update("Gambling", ctx.guild.id,
+                             "\"Casino\"", ctx.message.author.id, winnings)
             ef.money_transfer(ctx.message.author.id, winnings)
             ef.money_transfer("\"Casino\"", -winnings)
-            await ctx.send(f'Rolled a {result}. You win {winnings:.2f} dollars!')
+            await ctx.send(f'Rolled a {result}. \
+                             You win {winnings:.2f} dollars!')
         else:
-            ef.ledger_update("Gambling", ctx.guild.id, ctx.message.author.id, "\"Casino\"", bet)
+            ef.ledger_update("Gambling", ctx.guild.id, ctx.message.author.id,
+                             "\"Casino\"", bet)
             ef.money_transfer(ctx.message.author.id, -bet)
             ef.money_transfer("\"Casino\"", bet)
             await ctx.send(f'Rolled a {result}. You lose {bet:.2f} dollars!')
+
 
 @dice.error
 async def info_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send('Please enter a number from 1-6.')
 
-# $iscliff
 
+# $iscliff
 @client.command()
 async def iscliff(ctx, *, member: discord.Member):
     if (member.name == 'CliffRouge'):
@@ -141,65 +149,68 @@ async def iscliff(ctx, *, member: discord.Member):
     else:
         await ctx.send('Not chill, not chill!')
 
+
 @iscliff.error
 async def info_error(ctx, error):
     if isinstance(error, commands.BadArgument):
         await ctx.send('Thats like, not even a user on this server bro.')
     elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('Gimme a user and I can tell you if they are CliffRouge or not.')
+        await ctx.send('Gimme a user and I can tell you if \
+                        they are CliffRouge or not.')
 
-# Economy Commands 
+# Economy Commands
 
 # $balance
-
 @client.command()
 async def balance(ctx):
     balance = ef.check_balance(ctx.message.author.id)
-    await ctx.send(f'{ctx.message.author.name} has {"{:.2f}".format(round(balance, 2))} dollars.')
+    await ctx.send(f'{ctx.message.author.name} has \
+                     {"{:.2f}".format(round(balance, 2))} dollars.')
 
 
 # $give
-
 @client.command()
 async def give(ctx, member: discord.Member, amount: float):
     amount = round(amount, 2)
     if (ctx.message.author.id == 185902193595908096):
         ef.ledger_update("Test", ctx.guild.id, "\"Admin\"", member.id, amount)
         ef.money_transfer(member.id, amount)
-        await ctx.send(f'Gave {"{:.2f}".format(round(amount, 2))} dollars to {member.name}.')
+        await ctx.send(f'Gave {"{:.2f}".format(round(amount, 2))} \
+                         dollars to {member.name}.')
     else:
         await ctx.send(f'You must be a bot admin to do that.')
 
-# $pay
 
+# $pay
 @client.command()
 async def pay(ctx, member: discord.Member, amount: float):
     amount = round(amount, 2)
     gbalance = ef.check_balance(ctx.message.author.id)
-    if (amount <= 0): 
+    if (amount <= 0):
         await ctx.send(f'Please enter a positive amount!')
     elif (ctx.message.author.id == member.id):
         await ctx.send(f'You can\'t pay yourself!')
     elif (gbalance - amount < 0):
         await ctx.send(f'You do not have enough money!')
     else:
-        ef.ledger_update("User_Transfer", ctx.guild.id, ctx.message.author.id, member.id, amount)
+        ef.ledger_update("User_Transfer", ctx.guild.id,
+                         ctx.message.author.id, member.id, amount)
         ef.money_transfer(member.id, amount)
         ef.money_transfer(ctx.message.author.id, -amount)
-        await ctx.send(f'{ctx.message.author.name} paid {member.name} {"{:.2f}".format(round(amount, 2))} dollars.')
+        await ctx.send(f'{ctx.message.author.name} paid {member.name} \
+                         {"{:.2f}".format(round(amount, 2))} dollars.')
 
 
-# Bank Commands 
+# Bank Commands
 
 # $bank_balance
-
 @client.command()
 async def bank_balance(ctx):
     msg = bank.bank_balance(ctx.message.author)
     await ctx.send(msg)
 
-# $deposit
 
+# $deposit
 @client.command()
 async def deposit(ctx, amount: float):
     amount = round(amount, 2)
@@ -208,23 +219,24 @@ async def deposit(ctx, amount: float):
 
 
 # $withdraw
-
 @client.command()
 async def withdraw(ctx, amount: float):
     amount = round(amount, 2)
     msg = bank.new_withdrawal(ctx.message.author, amount, ctx.guild.id)
     await ctx.send(msg)
 
-# $withdraw
 
+# $withdraw
 @client.command()
 async def interest_rates(ctx):
-    await ctx.send(f'The deposit rate at the bank is currently {bank.deposit_rate * 100}%. The lending rate at the bank is currently {bank.lending_rate * 100}%.')
+    await ctx.send(f'The deposit rate at the bank is currently \
+                     {bank.deposit_rate * 100}%. The lending rate at \
+                     the bank is currently {bank.lending_rate * 100}%.')
+
 
 # Bank Proccesses
 
 # Check if interest needs to be paid out anytime a message is sent
-
 @client.event
 async def on_message(message):
     bank.handle_interest()
@@ -233,60 +245,62 @@ async def on_message(message):
 # Trading Commands
 
 # $quote
-
 @client.command()
 async def quote(ctx, stock_name):
     msg = trading.get_quote(stock_name)
     await ctx.send(msg)
 
-# $company_info
 
+# $company_info
 @client.command()
 async def company_info(ctx, stock_name):
     msg = trading.company_info(stock_name)
     await ctx.send(msg)
 
-# $buy
 
+# $buy
 @client.command()
-async def buy(ctx, stock_name, number:int):
-    msg = trading.buy_stock(stock_name, number, ctx.message.author, ctx.guild.id)
+async def buy(ctx, stock_name, number: int):
+    msg = trading.buy_stock(stock_name, number,
+                            ctx.message.author, ctx.guild.id)
     await ctx.send(msg)
+
 
 # $sell
-
 @client.command()
 async def sell(ctx, stock_name, number):
-    msg = trading.sell_stock(stock_name, number, ctx.message.author, ctx.guild.id)
+    msg = trading.sell_stock(stock_name, number,
+                             ctx.message.author, ctx.guild.id)
     await ctx.send(msg)
 
-# $portfolio
 
+# $portfolio
 @client.command()
 async def portfolio(ctx):
     embed = trading.check_portfolio(ctx.message.author)
     await ctx.send(embed=embed)
 
-# $buy_history
 
+# $buy_history
 @client.command()
 async def buy_history(ctx):
     embed = trading.buy_history(ctx.message.author)
     await ctx.send(embed=embed)
 
-# $sell_history
 
+# $sell_history
 @client.command()
 async def sell_history(ctx):
     embed = trading.sell_history(ctx.message.author)
     await ctx.send(embed=embed)
 
-# $order_history
 
+# $order_history
 @client.command()
 async def order_history(ctx):
     embed = trading.order_history(ctx.message.author)
     await ctx.send(embed=embed)
+
 
 # $portfolio_history
 @client.command()
@@ -296,7 +310,5 @@ async def portfolio_history(ctx):
         await ctx.send(file=discord.File('Graphs/graph.png'))
     else:
         ctx.send(msg)
-
-
 
 client.run(TOKEN)

economy_functions.py
@@ -1,6 +1,7 @@
 import sqlite3
 import datetime
 
+
 def check_balance(member_id):
     db = sqlite3.connect('main.sqlite')
     cursor = db.cursor()
@@ -18,22 +19,24 @@ def check_balance(member_id):
     db.close()
     return ans
 
-def money_transfer(member_id, amount): 
+
+def money_transfer(member_id, amount):
     db = sqlite3.connect('main.sqlite')
     cursor = db.cursor()
     cursor.execute('SELECT dollars FROM money WHERE user_id = ?', (member_id,))
     result = cursor.fetchone()
     if result is None:
-        sql = ("INSERT INTO money (user_id, dollars) VALUES(?, ?)")
+        sql = ('INSERT INTO money (user_id, dollars) VALUES(?, ?)')
         val = (member_id, amount)
     else:
         current_balance = result[0]
-        sql = ("UPDATE money SET dollars = ? WHERE user_id = ?")
+        sql = ('UPDATE money SET dollars = ? WHERE user_id = ?')
         val = (current_balance + amount, member_id)
     cursor.execute(sql, val)
     db.commit()
     cursor.close()
     db.close()
+
 
 def ledger_update(transaction_type, guild_id, giver_id, receiver_id, payment):
     db = sqlite3.connect('main.sqlite')
@@ -43,14 +46,15 @@ def ledger_update(transaction_type, guild_id, giver_id, receiver_id, payment):
     rbalance += payment
     db = sqlite3.connect('main.sqlite')
     cursor = db.cursor()
-    ledger_sql = ("INSERT INTO ledger (transaction_type, guild_id, giver_id, receiver_id, dollars, giver_balance, receiver_balance, date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")
-    ledger_val = (transaction_type, guild_id, giver_id, receiver_id, payment, gbalance, rbalance, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    ledger_sql = ('INSERT INTO ledger (transaction_type, guild_id, giver_id, \
+                                       receiver_id, dollars, giver_balance, \
+                                       receiver_balance, date) \
+                                       VALUES (?, ?, ?, ?, ?, ?, ?, ?)')
+    ledger_val = (transaction_type, guild_id, giver_id,
+                  receiver_id, payment,
+                  gbalance, rbalance,
+                  datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     cursor.execute(ledger_sql, ledger_val)
     db.commit()
     cursor.close()
     db.close()
-
-
-
-
-

commit f8fe8f2e03511acb89e72bfcdf1522372801cc14

More fixes to bugs introduced in 3307e2b
Strings were not formatted correctly

bank.py
@@ -99,7 +99,7 @@ def handle_interest():
             db = sqlite3.connect('main.sqlite')
             cursor = db.cursor()
             cursor.execute(f'UPDATE bank_deposits SET dollars = ' \
-                            'dollars * {(1 + DEPOSIT_RATE / 365) ** d}')
+                            f'dollars * {(1 + DEPOSIT_RATE / 365) ** d}')
             db.commit()
             with open("date.txt", "w+") as f:
                 f.write(datetime.date.today().strftime('%m/%d/%Y'))

bot.py
@@ -126,7 +126,7 @@ async def wagered_dice(ctx, choice, bet: float):
             ef.money_transfer(ctx.message.author.id, winnings)
             ef.money_transfer("\"Casino\"", -winnings)
             await ctx.send(f'Rolled a {result}. ' \
-                            'You win {winnings:.2f} dollars!')
+                            f'You win {winnings:.2f} dollars!')
         else:
             ef.ledger_update("Gambling", ctx.guild.id, ctx.message.author.id,
                              "\"Casino\"", bet)
@@ -165,7 +165,7 @@ async def info_error(ctx, error):
 async def balance(ctx):
     balance = ef.check_balance(ctx.message.author.id)
     await ctx.send(f'{ctx.message.author.name} has ' \
-                    '{"{:.2f}".format(round(balance, 2))} dollars.')
+                    f'{"{:.2f}".format(round(balance, 2))} dollars.')
 
 
 # $give
@@ -176,7 +176,7 @@ async def give(ctx, member: discord.Member, amount: float):
         ef.ledger_update("Test", ctx.guild.id, "\"Admin\"", member.id, amount)
         ef.money_transfer(member.id, amount)
         await ctx.send(f'Gave {"{:.2f}".format(round(amount, 2))} ' \
-                        'dollars to {member.name}.')
+                        f'dollars to {member.name}.')
     else:
         await ctx.send(f'You must be a bot admin to do that.')
 
@@ -229,9 +229,9 @@ async def withdraw(ctx, amount: float):
 # $withdraw
 @client.command()
 async def interest_rates(ctx):
-    await ctx.send(f'The deposit rate at the bank is currently ' \
-                    '{bank.deposit_rate * 100}%. The lending rate at ' \
-                    'the bank is currently {bank.lending_rate * 100}%.')
+    await ctx.send('The deposit rate at the bank is currently ' \
+                    f'{bank.deposit_rate * 100}%. The lending rate at ' \
+                    f'the bank is currently {bank.lending_rate * 100}%.')
 
 
 # Bank Proccesses

trading.py
@@ -82,7 +82,7 @@ def get_quote(stock_name):
                        interval="2m", auto_adjust=True, prepost=True)
     if (data.empty):
         return f"No results found for {stock_name}. " \
-               "Are you sure you have the right symbol?"
+               f"Are you sure you have the right symbol?"
     else:
         quote = round(data.tail(1)["Close"].values[0], 2)
         return f"The current quote for {stock_name} is {quote:.2f} dollars."
@@ -98,7 +98,7 @@ def buy_stock(stock_name, number, user, guild_id):
                        interval="2m", auto_adjust=True, prepost=True)
     if (data.empty):
         return f"No results found for {stock_name}. " \
-               "Are you sure you have the right symbol?"
+               f"Are you sure you have the right symbol?"
 
     # Get quote and payment
     quote = data.tail(1)["Close"].values[0]
@@ -110,12 +110,12 @@ def buy_stock(stock_name, number, user, guild_id):
         if (bal > 10):
             number_max = math.floor((bal - 10) / quote)
             return f"You do not have the required funds to buy {number} " \
-                   "shares of {stock_name}! The maximum number of shares " \
-                   "you can afford is {number_max}..."
+                   f"shares of {stock_name}! The maximum number of shares " \
+                   f"you can afford is {number_max}..."
         else:
             return f"You do not have the required funds to buy {number} " \
-                   "shares of {stock_name}! You have less than " \
-                   "{BROKERAGE_FEE:.2f} dollars, the brokerage fee."
+                   f"shares of {stock_name}! You have less than " \
+                   f"{BROKERAGE_FEE:.2f} dollars, the brokerage fee."
 
     # Update stock ledger
     stock_ledger_update("\"Buy Order\"", guild_id, user.id,
@@ -127,9 +127,9 @@ def buy_stock(stock_name, number, user, guild_id):
     ef.money_transfer("\"Brokerage\"", BROKERAGE_FEE)
     add_to_portfolio(user.id, stock_name, number)
     return f"{user.name} bought {number} shares of {stock_name} at " \
-            "{quote:.2f} dollars each. The total value of the transaction was " \
-            "{payment:.2f} dollars, plus the {BROKERAGE_FEE:.2f} " \
-            "dollar brokerage fee."
+            f"{quote:.2f} dollars each. The total value of the transaction was " \
+            f"{payment:.2f} dollars, plus the {BROKERAGE_FEE:.2f} " \
+            f"dollar brokerage fee."
 
 
 def sell_stock(stock_name, number, user, guild_id):
@@ -189,9 +189,9 @@ def sell_stock(stock_name, number, user, guild_id):
     ef.money_transfer(user.id, total_owed)
     ef.money_transfer("\"Brokerage\"", BROKERAGE_FEE)
     return f"{user.name} sold {number} shares of {stock_name} at {quote:.2f} " \
-            "dollars each. The total value of the transaction was " \
-            "{payment:.2f} dollars, minus the {BROKERAGE_FEE:.2f} " \
-            "dollar brokerage fee."
+            f"dollars each. The total value of the transaction was " \
+            f"{payment:.2f} dollars, minus the {BROKERAGE_FEE:.2f} " \
+            f"dollar brokerage fee."
 
 
 def check_portfolio(user):
@@ -227,7 +227,7 @@ def check_portfolio(user):
                         value=f"{amount} | {price:.2f} | {value:.2f}",
                         inline=True)
     embed.set_footer(text=f"The total value of your portfolio is " \
-                           "{total_value:.2f} dollars!")
+                           f"{total_value:.2f} dollars!")
     return embed
 
 